9fd4b2d8f6d324af90b1a92fbd184964edf0426c
Juan Covarrubias <me@juan.fi>
2022-01-18 17:50:26 +0200

    Edit puhelinluettelo frontend

--- osa02/puhelinluettelo/src/App.js
+++ osa02/puhelinluettelo/src/App.js
@@ -3,12 +3,25 @@ import PersonTable from './components/PersonTable'
 import PersonInputForm from './components/PersonInputForm'
 import FilterInput from './components/FilterInput'
 import personsService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterByName, setFilterByName] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState('')
+  const [notificationType, setNotificationType] = useState(null)
+
+  const setNotification = (message, type) => {
+    setNotificationMessage(message)
+    setNotificationType(type)
+
+    setTimeout(() => {
+      setNotificationMessage('')
+      setNotificationType(null)
+    }, 5000)
+  }
 
 
   useEffect(() => {
@@ -24,9 +37,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <FilterInput filterByName={filterByName} setFilterByName={setFilterByName} />
       <h3>Add a new</h3>
-      <PersonInputForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
+      <PersonInputForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} setNotification={setNotification} />
       <h2>Numbers</h2>
       <PersonTable persons={persons} setPersons={setPersons} filterByName={filterByName} />
+      <Notification message={notificationMessage} type={notificationType} />
     </div>
   )
 
