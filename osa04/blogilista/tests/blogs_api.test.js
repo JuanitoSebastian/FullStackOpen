376e012fdd819b754dba37f83791288e9ca40926
Juan Covarrubias <me@juan.fi>
2022-01-20 16:58:38 +0200

    Ex 4.10, renamed __tests__ folder to tests

---
+++ osa04/blogilista/tests/blogs_api.test.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogToCreate = new Blog(helper.initalBlogs[0])
+  await blogToCreate.save()
+  blogToCreate = new Blog(helper.initalBlogs[1])
+  await blogToCreate.save()
+})
+
+describe('api get tests', () => {
+  test('correct blogs returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initalBlogs.length)
+  })
+
+  test('blogs are formatted correctly', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+      expect(blog._id).toBeUndefined()
+      expect(blog.__v).toBeUndefined()
+    })
+  })
+})
+
+describe('api post tests', () => {
+  test('adding a blog works correctly', async () => {
+    const blogToAdd = {
+      title: 'Basics of Swift programming',
+      author: 'Tim Apple',
+      url: 'https://icloud.com',
+      likes: 100
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogToAdd)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAfterAdding = await helper.blogsInDb()
+    expect(blogsAfterAdding).toHaveLength(helper.initalBlogs.length + 1)
+
+    const contents = blogsAfterAdding.map(blog => blog.title)
+    expect(contents).toContain(blogToAdd.title)
+
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
