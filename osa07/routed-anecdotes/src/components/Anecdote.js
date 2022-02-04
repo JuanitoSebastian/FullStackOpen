1b09642c6e73d5cf2175622f73cc1682032cbb45
Juan Covarrubias <me@juan.fi>
2022-02-04 09:44:07 +0200

    Ex 7.3

---
+++ osa07/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content}</h2>
+    <p>Has {anecdote.votes} votes</p>
+    <p>For more info see <a href={anecdote.info} target='_blank' rel='noreferrer'>{anecdote.info}</a></p>
+  </div>
+)
+
+export default Anecdote
