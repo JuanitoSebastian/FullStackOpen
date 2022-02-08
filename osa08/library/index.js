74dfc34cc936f31707fb83edbd8587ac8f1168ad
Juan Covarrubias <me@juan.fi>
2022-02-08 12:52:18 +0200

    Ex 8.15

--- osa08/library/index.js
+++ osa08/library/index.js
@@ -1,94 +1,24 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const { v1: uuid } = require('uuid')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const Book = require('./models/book')
+const Author = require('./models/author')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-]
-
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution']
-  },
-]
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB: ', error.message)
+  })
 
 const typeDefs = gql`
   type Book {
     title: String!,
     published: Int!,
-    author: String!
+    author: Author!,
     genres: [String!]!,
     id: ID!
   },
@@ -120,43 +50,74 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      const booksToReturn = args.author
-        ? books.filter(book => book.author === args.author)
-        : books
-
-      return args.genre
-        ? booksToReturn.filter(book => book.genres.includes(args.genre))
-        : booksToReturn
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const allBooks = await Book.find({})
+        .populate('author', { id: 1, name: 1, born: 1 })
+      return allBooks
     },
-    allAuthors: () => authors
+    allAuthors: async () => {
+      const fetchedAuthors = await Author.find({})
+      return fetchedAuthors
+    }
   },
   Author: {
-    bookCount: (root) => {
-      const booksByAuthor = books.filter(book => book.author === root.name)
+    bookCount: async (root) => {
+      const booksByAuthor = await Book.find({ author: root._id })
       return booksByAuthor.length
     }
   },
   Mutation: {
-    addBook: (root, args) => {
-      const bookToCreate = { ...args, id: uuid() }
-      books = [...books, bookToCreate]
-      if (!authors.find(author => author.name === bookToCreate.author)) {
-        const authorToCreate = { name: bookToCreate.author, id: uuid() }
-        authors = [...authors, authorToCreate]
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+
+      // Create author if needed
+      if (!author) {
+        try {
+          author = new Author({
+            name: args.author
+          })
+          await author.save()
+        } catch (error) {
+          throw new UserInputError(error.message, {
+            invalidArgs: args
+          })
+        }
       }
-      return bookToCreate
+
+      try {
+        const book = new Book({
+          title: args.title,
+          published: args.published,
+          author: author._id
+        })
+
+        const savedBook = await book.save()
+        await Book.populate(book, { path: 'author' })
+        return savedBook
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
+      }
     },
-    editAuthor: (root, args) => {
-      const authorToEdit = authors.find(author => author.name === args.name)
-      if (!authorToEdit) { return null }
+    editAuthor: async (root, args) => {
+      const authorToEdit = await Author.findOne({ name: args.name })
+      if (!authorToEdit) {
+        throw new UserInputError('Author was not found!')
+      }
+      authorToEdit.born = args.setBornTo
 
-      const updatedAuthor = { ...authorToEdit, born: args.setBornTo }
-      authors = authors.map(author => author.id === updatedAuthor.id ? updatedAuthor : author)
+      try {
+        await authorToEdit.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
+      }
 
-      return updatedAuthor
+      return authorToEdit
     }
   }
 }
