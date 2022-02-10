5621e4537269ad8b76ec5f8256b07e629026fe91
Juan Covarrubias <me@juan.fi>
2022-02-10 13:23:06 +0200

    Ex 9.5

--- osa09/first-ts-project/bmiCalculator.ts
+++ osa09/first-ts-project/bmiCalculator.ts
@@ -1,39 +1,8 @@
-interface BmiValues {
-  height: number;
-  weight: number;
-}
-
-const parseArguments = (args: Array<string>): BmiValues => {
-  if (args.length !== 4) {
-    throw new Error('Please provide 2 values: height and weight');
-  }
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
-    }
-  }
-  
-  throw new Error('Provided values were not numbers!');
-}
-
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   height = height * 0.01;
   const bmi: number = weight / Math.pow(height, 2);
 
   if (bmi > 24.9) { return 'Overweight'; }
   if (bmi < 18.5) { return 'Underweight'; }
   return 'Normal (healthy weight)';
-}
-
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = 'Something happened';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
 }
---
+++ osa09/first-ts-project/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
+const app = express();
+
+app.get('/hello', (_request, response) => {
+  response.send('Hello Full Stack! 🥰');
+});
+
+app.get('/bmi', (request, response) => {
+  const height = Number(request.query.height);
+  const weight = Number(request.query.weight);
+  if (isNaN(height) || isNaN(weight)) {
+    return response.status(400).json({ error: 'malformatted parameters' })
+  }
+  
+  return response.status(200).json({
+    weight,
+    height,
+    bmi: calculateBmi(height, weight)
+  })
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
